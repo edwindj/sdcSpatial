1cc79627effe2206514f84c66dffe34cf9d04a06
Edwin de Jonge <edwindjonge@gmail.com>
2019-01-22 14:36:51 +0100

    updating stuff

--- R/raster_at.R
+++ R/raster_at.R
@@ -1,10 +1,12 @@
 #' Get a raster at a certain resolution
 #'
 #' Get a raster at a certain resolution (with rounded coordinates)
+#' @param x geographical object (that supports extent)
+#' @param res desired resolution (default 100).
 #' @export
 get_raster_at <- function(x, res = 100){
-    ext <- extent(x)[] / res
+    ext <- raster::extent(x)[] / res
     ext[c(1,3)] <- floor(ext[c(1,3)])
     ext[c(2,4)] <- ceiling(ext[c(2,4)])
-    raster(extent(res*ext), res = res)
+    raster::raster(raster::extent(res*ext), res = res)
 }
--- R/smooth.R
+++ R/smooth.R
@@ -1,15 +1,17 @@
 #' Create kde density version of a raster
 #'
+#' Create kde density version of a raster
 #' @param x raster object
 #' @param bw bandwidth
 #' @param na.rm should the `NA` value be removed from the raster?
+#' @param pad should the data be padded.
 #' @param ... passed through to focal
 #' @param threshold cells with a lower (weighted) value of this threshold will be removed.
 #' @export
-smooth <- function(x, bw = res(x), na.rm = TRUE, pad = TRUE, threshold = 10, ...){
-  w <- focalWeight(x, bw)
+smooth <- function(x, bw = raster::res(x), na.rm = TRUE, pad = TRUE, threshold = 10, ...){
+  w <- raster::focalWeight(x, bw)
   s <- threshold * max(w)
-  x_s <- focal(x, w = w, na.rm = na.rm, pad = pad, ...)
+  x_s <- raster::focal(x, w = w, na.rm = na.rm, pad = pad, ...)
   #print(sum(x_s[x_s < 10*s]))
   is.na(x_s) <- x_s < s
   x_s
