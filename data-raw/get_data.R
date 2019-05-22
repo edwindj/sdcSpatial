8db1b5bbb5253a82730018bcba94ce4a12cfd09f
Edwin de Jonge <edwindjonge@gmail.com>
2019-05-22 20:01:27 +0200

    adding some raster data etc

--- data-raw/get_data.R
+++ data-raw/get_data.R
@@ -11,12 +11,31 @@ if (!file.exists("data-raw/bagadres.csv")){
 
 d <- data.table::fread(
     "data-raw/bagadres-full.csv"
-  , nrows = 1e4
+  #, nrows = 1e5
   , select = c("verblijfsobjectgebruiksdoel", "x", "y")
 )
 
-e <- d[,.(doel = strsplit(verblijfsobjectgebruiksdoel,", ")
-             , row = seq_len(.N))]
-unnest(e)
-library(tidyverse)
-View(d)
+e <- d[, doel := strsplit(verblijfsobjectgebruiksdoel,", ")]
+#e <- e[, .(doel = unlist(doel), x = x, y = y)]
+
+doelen <- unique(unlist(e$doel))
+
+for (doel in doelen){
+  d[, c(doel) := grepl(doel, verblijfsobjectgebruiksdoel)]
+}
+
+dir.create("data", recursive = TRUE, showWarnings = FALSE)
+
+offices <- d[kantoorfunctie==TRUE,.(x,y)]
+residences <- d[woonfunctie==TRUE, .(x,y)]
+
+use_data(offices)
+use_data(residences)
+
+# TODO add simulated data for several cities.
+# Idea, draw a value from a distribution for all population members.
+# select at random 10% focal points
+# assign 90% of the other values to nearby locations: thereby introducing the spatial
+# component
+
+
