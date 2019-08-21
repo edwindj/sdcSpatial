af29c5bbe01a44e0be2a0e02ec1e7015509a4f1f
Edwin de Jonge <edwindjonge@gmail.com>
2019-08-21 08:27:01 +0200

    updating documentation

--- R/is_sensitive_at.R
+++ R/is_sensitive_at.R
@@ -1,6 +1,8 @@
-#' Calculate sensitivity from  raster at x,y locations.
+#' Calculate sensitivity from  a sdc_raster at x,y locations.
 #'
-#' Calculate sensitivity from  raster at x,y locations.
+#' Calculate sensitivity from  a sdc_raster at x,y locations.
+#' A typical use is to calculate the sensitivity for each of the locations `x`
+#' was created with (see example).
 #' @param x [sdc_raster()]
 #' @param xy matrix of x and y coordinates, or a SpatialPoints or
 #' SpatialPointsDataFrame object
@@ -10,6 +12,7 @@
 #' @family sensitive
 #' @export
 is_sensitive_at <- function(x, xy, ...){
+  assert_sdc_raster(x)
   sens <- is_sensitive(x, ...)
   as.logical(raster::extract(sens, xy))
   # cell <- raster::cellFromXY(sens, xy)
