e1c983c3a789477f0364d399c4b6cb2066be751a
Edwin de Jonge <edwindjonge@gmail.com>
2019-08-21 08:31:53 +0200

    tweaking

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# sdcSpatial 0.2.0.9000
+
+* Added a `NEWS.md` file to track changes to the package.
+* Added `is_sensitive_at` function to calculate sensitivity for original locations.
+* Added `protect_coarsen` function for aggregating the raster.
---
+++ R/protect_coarsen.R
@@ -0,0 +1,30 @@
+protect_coarsen <- function(x, fact = 2, ...){
+  assert_sdc_raster(x)
+  aggregate.sdc_raster(x, fact = fact, ...)
+}
+
+aggregate.sdc_raster <- function(x, fact = 2, ...){
+  # copy sdc_raster info
+  x_agg <- x
+  value <- raster::aggregate( x$value[[c("sum", "count")]]
+                            , fact = fact, fun = "sum")
+  value$mean <- value$sum / value$count
+  if (x$type == "numeric"){
+    value$max <- raster::aggregate(x$value$max, fact = fact, fun = "max")
+    value$max2 <- raster::aggregate(x$value$max2, fact = fact, fun = "max2")
+  }
+
+  value <- value[[names(x$value)]]
+
+  x_agg$value <- value
+  x_agg$scale <- 1 #TODO fix scale
+  x_agg
+}
+
+# prod <- sdc_raster(enterprises, enterprises$production)
+# x <- prod
+# aggregate(prod)
+# aggregate(x, fact = 4)
+# fined <- sdc_raster(enterprises, enterprises$fined, min_count=5)
+# x <- fined
+# aggregate(x)
