5d14c6775461fbd975eb11a81cbfc6127631dfd6
Edwin de Jonge <edwindjonge@gmail.com>
2022-03-24 11:27:23 +0100

    donttest is_sensitive_at

--- example/is_sensitive_at.R
+++ example/is_sensitive_at.R
@@ -1,4 +1,6 @@
+\donttest{
 production <- sdc_raster(enterprises, "production")
 
 # add the sensitive variable to original data set.
 enterprises$sensitive <- is_sensitive_at(production, enterprises)
+}
