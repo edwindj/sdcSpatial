59459a9c11dc39456f4928c643f6b5f56eb60764
Edwin de Jonge <edwindjonge@gmail.com>
2022-03-24 12:03:38 +0100

    updating documentation

--- example/remove_sensitive.R
+++ example/remove_sensitive.R
@@ -1,3 +1,4 @@
+\donttest{
 library(raster)
 
 unemployed <- sdc_raster(dwellings[1:2], dwellings$unemployed, r=200)
@@ -10,3 +11,4 @@ unemployed_safe <- remove_sensitive(unemployed, risk_type="discrete")
 plot_sensitive(unemployed_safe, zlim=c(0,1))
 print(unemployed)
 unemployed$value
+}
